7ef2cc5f896b6922df2ea642990e6b2ebac5a083
Newton-Maurya <newtonmauryadw@gmail.com>
2021-07-20 17:39:08 +0530

    s4 cohmmite

--- server.js
+++ server.js
@@ -39,9 +39,10 @@ const StateDistrictName = mongoose.model('statedistrictname', StateDistrictNameS
 // Routing
 
 
- if(process.env.NODE.ENV === "production"){
-     app.use(express.static("client/build"))
- }
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 app.listen(port, () => {
     console.log(`Succeddfully connected on port ${port}`)
 }
